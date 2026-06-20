7672eff6acfbfa0a48c59663d40a1168b87d4b68
GUST050 <161457819+GUST050@users.noreply.github.com>
2026-06-20 21:22:06 +0200

    Update test_webhook_target.py

--- src/test_webhook_target.py
+++ src/test_webhook_target.py
@@ -15,6 +15,7 @@ def login(username, password):
     query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
     cursor.execute(query)
     return cursor.fetchone()
+    
 
 
 def get_user_data(user_id):
